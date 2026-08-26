835cb0043791514a7d082adf1d0c08ec4ec0af70
Chriss Furenes <chrissof@uia.no>
2026-08-26 19:09:52 +0200

    sorting and added new package to keep tings more clean

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,1 +1,30 @@
 package cmd
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+)
+
+func UserHomeDir() string {
+	if runtime.GOOS == "windows" {
+		home := os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
+		if home == "" {
+			home = os.Getenv("USERPROFILE")
+		}
+		return filepath.Clean(home)
+	}
+	return filepath.Clean(os.Getenv("HOME"))
+}
+
+func KubePath() string {
+	return filepath.Join(UserHomeDir(), ".kube")
+}
+
+func Help() {
+	fmt.Println("KCC usage:")
+	fmt.Println("version ........ prints version")
+	fmt.Println("i/import ....... import new config")
+	fmt.Println("help/h .......... show help")
+}
--- cmd/version.go
+++ cmd/version.go
@@ -1,5 +1,5 @@
 package cmd
 
-func version() string{
-	return "0.7.0beta"
+func Version() string {
+	return "0.8.0"
 }
